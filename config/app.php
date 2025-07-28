a0f119b01463fa30aa28948f7b338932fcbe82dd
Craig Zearfoss <craigzearfoss@yahoo.com>
2025-07-28 13:02:26 -0400

    cleanup of copyrights and other small changes

--- config/app.php
+++ config/app.php
@@ -13,7 +13,9 @@ return [
     |
     */
 
-    'name' => env('APP_NAME', 'Laravel'),
+    'copyright' => env('APP_COPYRIGHT', ''),
+    'name'      => env('APP_NAME', 'Laravel'),
+    'owner'     => env('APP_OWNER', ''),
 
     /*
     |--------------------------------------------------------------------------
